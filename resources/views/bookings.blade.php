c4b70a82e0bed20fafc0b399095e5477d0998174
nevinjames2001 <74431298+nevinjames2001@users.noreply.github.com>
2022-12-04 02:11:16 -0500

    delpo

--- resources/views/bookings.blade.php
+++ resources/views/bookings.blade.php
@@ -90,7 +90,7 @@
                 <select class='form-control' id='veterinarian_select' name='veterinarian'>
                     <option>Select Doctor</option>
                     @foreach($data as $d)
-                        <option value="{{$d['veterinarian_id']}}">{{$d['veterinarian_name']}}</option>
+                        <option value="{{$d['id']}}">{{$d['veterinarian_name']}}</option>
 
                     @endforeach
                 </select>
