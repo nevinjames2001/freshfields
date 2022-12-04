affe2cd0b3921e4ff3ce94594c51fb085788a548
nevinjames2001 <74431298+nevinjames2001@users.noreply.github.com>
2022-12-04 02:13:12 -0500

    delpo

--- resources/views/bookings.blade.php
+++ resources/views/bookings.blade.php
@@ -135,19 +135,19 @@
                         echo "<td><h4>$currentDay</h4><button class='btn btn-danger btn-xs'>N/A</button>";
                     }
                     else{
-                        $totalbookings =App\Http\Controllers\BookingsController::checkSlots($date,$year,$veterinarian);
-                        // $totalbookings=checkSlots($mysqli,$date,$year,$first_veterinarian);
+                        // $totalbookings =App\Http\Controllers\BookingsController::checkSlots($date,$year,$veterinarian);
+                        // // $totalbookings=checkSlots($mysqli,$date,$year,$first_veterinarian);
 
-                        if($totalbookings==8){
-                            echo "<td class='$today'><h4>$currentDay</h4><a href='#' class='btn btn-danger btn-xs'>All Booked</a>";
-                        }
-                        else{
-                            $availabelslots=8-$totalbookings;
+                        // if($totalbookings==8){
+                        //     echo "<td class='$today'><h4>$currentDay</h4><a href='#' class='btn btn-danger btn-xs'>All Booked</a>";
+                        // }
+                        // else{
+                        //     $availabelslots=8-$totalbookings;
 
-                            ?>
-                            <td class='$today'><h4>{{$currentDay}}</h4><a href="{{url('book')}}/{{$date}}/{{$veterinarian}}" class='btn btn-success btn-xs'>Book</a><small><i>{{$availabelslots}} slots left</i></small>;
-                            <?php
-                        }
+                        //     ?>
+                        //     <td class='$today'><h4>{{$currentDay}}</h4><a href="{{url('book')}}/{{$date}}/{{$veterinarian}}" class='btn btn-success btn-xs'>Book</a><small><i>{{$availabelslots}} slots left</i></small>;
+                        //     <?php
+                        // }
                     }
                     echo "</td>";
                     $currentDay++;
