d7482c63286e2c3bc6c2b2f05d2800ddc3ef36fd
nevinjames2001 <74431298+nevinjames2001@users.noreply.github.com>
2022-12-06 01:11:43 -0500

    Update bookingDisplay.blade.php

--- resources/views/bookingDisplay.blade.php
+++ resources/views/bookingDisplay.blade.php
@@ -48,30 +48,7 @@ $veterinarian_id=Auth::user()->veterinarian_id;
         @endforeach
 </table>
 
-<div class="bookOff">
-    <form action="{{url('bookOff')}}/">
-        <div class="form-group">
-            <label for="date" >Date<label>
-            <input type="date" class="form-control" id="date" name="date">
-        </div>
-        <div class="form-group">
-            <label for="timeslot" >Time<label>
-            <select id="timeslot" name="timeslot">
 
-                @foreach($slot as $s)
-                    <option value="{{$s}}">{{$s}}</option>
-                @endforeach
-            </select>
-        </div>
-        <input type="hidden" name="name" value="{{ Auth::user()->name}}">
-        <input type="hidden" name="email" value="{{ Auth::user()->email}}">
-        <input type="hidden" name="phone" value="{{ Auth::user()->phone}}">
-        <input type="hidden" name="phone" value="2896983628">
-        <input type="hidden" name="veterinarian_id" value="{{ Auth::user()->veterinarian_id}}">
-        <input type="hidden" name="reason" value="Book Off">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
 <body>
 </html>
 @endsection('content')
